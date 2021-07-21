f425019250b9e136fd96a60944d355b931971766
s.n <n.shriya@outlook.com>
2021-07-20 17:52:11 -0700

    Changed home page layout to match wireframe. Create format for log waste page. Began adding resources to get resources page.

--- KWK Final Project - Ecotic/GetResourcesViewController.swift
+++ KWK Final Project - Ecotic/GetResourcesViewController.swift
@@ -15,7 +15,27 @@ class GetResourcesViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func NRDCbtn(_ sender: UIButton) {
+        let NRDC = URL (string: "https://www.nrdc.org/food-waste")
+        UIApplication.shared.open(NRDC!)
+    }
+    
+    @IBAction func USDAbtn(_ sender: UIButton) {
+        let USDA = URL (string: "https://www.usda.gov/foodwaste/faqs")
+        UIApplication.shared.open(USDA!)
+    }
+    
+    @IBAction func RescuingLeftoverbtn(_ sender: UIButton) {
+        let RescuingLeftoverCuisine = URL (string: "https://www.rescuingleftovercuisine.org/challenge?gclid=Cj0KCQjwxdSHBhCdARIsAG6zhlW1keGIhSt0BaIEjnMLEMN-kpQ7lJzz8i4aIW9fzHEw-uE3rdi9VToaAnHiEALw_wcB")
+        UIApplication.shared.open(RescuingLeftoverCuisine!)
+    }
+    
+    @IBAction func FoodPrintbtn(_ sender: UIButton) {
+        let FoodPrint = URL (string: "https://foodprint.org/issues/the-problem-of-food-waste/")
+        UIApplication.shared.open(FoodPrint!)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
