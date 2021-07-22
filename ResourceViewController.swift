2c013e2d00b710fcc1e67297f808ea414dc3c28d
Joanne Lin <joannee.lin@gmail.com>
2021-07-22 11:44:10 -0700

    basic code for scroll

---
+++ ResourceViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ResourceViewController.swift
+//  KWK Final Project - Ecotic
+//
+//  Created by Joanne Lin on 7/22/21.
+//
+
+import UIKit
+
+class ResourceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func USDAbutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func NRDCbutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func rescueButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func printButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func OLIObutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func RTSbutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func americabutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tankButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func earthButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func grundButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func harvestButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func wastebutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func medicalButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func print2Button(_ sender: UIButton) {
+    }
+    
+    @IBAction func hennepinButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func healthlineButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func USFDAbutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func BBCbutton(_ sender: UIButton) {
+    }
+    
+    @IBAction func NRDC2button(_ sender: UIButton) {
+    }
+    
+    @IBAction func USFAObutton(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
